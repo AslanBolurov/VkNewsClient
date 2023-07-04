9c01a816405c0ea05280ed3737e5b4e262efad50
AslanBolurov <aslanbolurov@list.ru>
2023-07-04 19:09:23 +0300

    material scaffold

--- app/src/main/java/com/sumincourse/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/sumincourse/vknewsclient/MainActivity.kt
@@ -3,36 +3,75 @@ package com.sumincourse.vknewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.sumin.vknewsclient.ui.theme.PostCard
-import com.sumincourse.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VkNewsClientTheme {
-                // A surface container using the 'background' color from the theme
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colorScheme.background)
-                        .padding(8.dp),
-                ) {
-                    PostCard()
-                }
-            }
+            Test()
         }
     }
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Test() {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("TopAppBar title") },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Menu, contentDescription = null)
+                    }
+                },
+            )
+        },
+        bottomBar = {
+            BottomNavigation {
+                BottomNavigationItem(
+                    selected = true,
+                    onClick = {},
+                    icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                    label = { Text("Favourite")}
+                )
+            }
+        },
+        drawerContent = {
+            Text(text = "Text 1")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Text 2")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Text 3")
+        }
+
+
+    ) {
+        Text(
+            modifier = Modifier.padding(it),
+            text = "This is scaffold content"
+        )
+    }
+
+}
+
 
